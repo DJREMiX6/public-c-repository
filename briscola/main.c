4348e33fcaa59e268ea444fb114ac1862f11dae5
DJREMiX6 <francescogirellilime@gmail.com>
2020-11-30 06:48:35 +0100

    Added first projects

--- briscola/main.c
+++ briscola/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <time.h>
 
 //CONSTANTS
+//Constant used to print debug log on STDOUT
 const bool DEBUG = true;
-#include <stdio.h>
-#include <stdlib.h>
 
+//ENUMS
+//Enumerator to define card's seed
 enum Seed {
     Null,
     Spade,
@@ -15,12 +17,17 @@ enum Seed {
     Coppe
 };
 
+//STRUCTS
+//Struct to define a card
 struct Card {
+    //Pointer to the Seed of the card
     enum Seed* seed;
+    //Pointer to the value of the cart (min 1, max 10)
     int* value;
 };
 
-//CONST METHODS
+//METHODS
+//this method return a "String" based on the Seed passed or an error message if the value is minor than 0 and major than 4
 const char* getSeedName(enum Seed seme) {
     static char* semi[] = {"Null", "Spade", "Denari", "Bastoni", "Coppe"};
     static char* errMess = "Error!";
@@ -31,6 +38,7 @@ const char* getSeedName(enum Seed seme) {
     return semi[semeVal];
 }
 
+//Method called in newDeck() function if there is an error during the allocation of the deck
 void freeDeckInLoop(struct Card* deck, int errorIndex) {
     if(deck != NULL) {
         printf("Freeing space of deck in loop cause of an error...");
@@ -43,7 +51,7 @@ void freeDeckInLoop(struct Card* deck, int errorIndex) {
                 free((deck + i)->value);
             }
         }
-        
+
         free(deck);
         printf("Space free.");
     } else {
@@ -51,7 +59,10 @@ void freeDeckInLoop(struct Card* deck, int errorIndex) {
     }
 }
 
+//Create a new deck and return the pointer of the deck's first element
+//The deck is an array of type struct Card
 struct Card* newDeck() {
+    //allocating the memory for the deck
     struct Card* deck = malloc(sizeof(struct Card) * 40);
     if(deck == NULL) {
         if(DEBUG == true) {
@@ -60,12 +71,13 @@ struct Card* newDeck() {
         return NULL;
     }
 
-    int tempVal = 1;
+    int* tempVal = malloc(sizeof(int));
+    *(tempVal) = 1;
     for(int i = 0; i < 40; i++) {
         //Inserimento Spade
         if(i < 10) {
             if(DEBUG == true) {
-                printf("Try to alloc memory for card:\n\tSeed: Spade,\tValue: %d\n", i);
+                printf("Try to alloc memory for card:\n\tSeed: Spade,\tValue: %d\n", *(tempVal));
             }
             //Try to alloc memory
             (deck + i)->seed = malloc(sizeof(Spade));
@@ -83,7 +95,7 @@ struct Card* newDeck() {
             (*((deck + i)->seed)) = Spade;
             
             //Try to alloc memory
-            (deck + i)->value = malloc(sizeof(tempVal));
+            (deck + i)->value = malloc(sizeof(*tempVal));
             //If there is an error during memory allocation then Free the memory from all the Cards
             if((deck + i)->value == NULL) {
                 if(DEBUG == true) {
@@ -95,12 +107,12 @@ struct Card* newDeck() {
             if(DEBUG == true) {
                 printf("Card's value succesfully allocated at address:%p\n", (deck + i)->value);
             }
-            (*((deck + i)->value)) = tempVal;
+            (*((deck + i)->value)) = *tempVal;
         } 
         //Inserimento Denari
         else if(i < 20) {
             if(DEBUG == true) {
-                printf("Try to alloc memory for card:\n\tSeed: Denari,\tValue: %d\n", i);
+                printf("Try to alloc memory for card:\n\tSeed: Denari,\tValue: %d\n", *tempVal);
             }
             //Try to alloc memory
             (deck + i)->seed = malloc(sizeof(Denari));
@@ -118,7 +130,7 @@ struct Card* newDeck() {
             (*((deck + i)->seed)) = Denari;
 
             //Try to alloc memory
-            (deck + i)->value = malloc(sizeof(tempVal));
+            (deck + i)->value = malloc(sizeof(*tempVal));
             //If there is an error during memory allocation then Free the memory from all the Cards
             if((deck + i)->value == NULL) {
                 if(DEBUG == true) {
@@ -130,12 +142,12 @@ struct Card* newDeck() {
             if(DEBUG == true) {
                 printf("Card's value succesfully allocated at address:%p\n", (deck + i)->value);
             }
-            (*((deck + i)->value)) = tempVal;
+            (*((deck + i)->value)) = *tempVal;
         } 
         //Inserimento Bastoni
         else if(i < 30) {
             if(DEBUG == true) {
-                printf("Try to alloc memory for card:\n\tSeed: Bastoni,\tValue: %d\n", i);
+                printf("Try to alloc memory for card:\n\tSeed: Bastoni,\tValue: %d\n", *tempVal);
             }
             //Try to alloc memory
             (deck + i)->seed = malloc(sizeof(Bastoni));
@@ -153,7 +165,7 @@ struct Card* newDeck() {
             (*((deck + i)->seed)) = Bastoni;
 
             //Try to alloc memory
-            (deck + i)->value = malloc(sizeof(tempVal));
+            (deck + i)->value = malloc(sizeof(*tempVal));
             //If there is an error during memory allocation then Free the memory from all the Cards
             if((deck + i)->value == NULL) {
                 if(DEBUG == true) {
@@ -165,12 +177,12 @@ struct Card* newDeck() {
             if(DEBUG == true) {
                 printf("Card's value succesfully allocated at address:%p\n", (deck + i)->value);
             }
-            (*((deck + i)->value)) = tempVal;
+            (*((deck + i)->value)) = *tempVal;
         } 
         //Inserimento Coppe
         else {
             if(DEBUG == true) {
-                printf("Try to alloc memory for card:\n\tSeed: Coppe,\tValue: %d\n", i);
+                printf("Try to alloc memory for card:\n\tSeed: Coppe,\tValue: %d\n", *tempVal);
             }
             //Try to alloc memory
             (deck + i)->seed = malloc(sizeof(Coppe));
@@ -188,7 +200,7 @@ struct Card* newDeck() {
             (*((deck + i)->seed)) = Coppe;
 
             //Try to alloc memory
-            (deck + i)->value = malloc(sizeof(tempVal));
+            (deck + i)->value = malloc(sizeof(*tempVal));
             //If there is an error during memory allocation then Free the memory from all the Cards
             if((deck + i)->value == NULL) {
                 if(DEBUG == true) {
@@ -200,21 +212,24 @@ struct Card* newDeck() {
             if(DEBUG == true) {
                 printf("Card's value succesfully allocated at address:%p\n", (deck + i)->value);
             }
-            (*((deck + i)->value)) = tempVal;
+            (*((deck + i)->value)) = *tempVal;
         }
 
-        tempVal ++;
+        *tempVal = (*tempVal) + 1;
 
         if(i == 9 || i == 19 || i == 29) {
-            tempVal = 1;
+            *tempVal = 1;
         }
     }
+
+    free(tempVal);
     if(DEBUG == true) {
         printf("Deck created!\n");
     }
     return deck;
 }
 
+//Method that free the memory of the entire deck
 void disposeDeck(struct Card* deck) {
     if(DEBUG == true) {
         printf("Disposing deck at address: %p\n", deck);
@@ -241,7 +256,7 @@ void disposeDeck(struct Card* deck) {
             }
         }
         free(deck);
-        printf("Memory free.\n");
+        printf("Deck disposed.\nMemory free.\n");
     } else {
         if(DEBUG == true) {
             printf("Error during disposing the deck, deck is NULL!\n");
@@ -249,6 +264,7 @@ void disposeDeck(struct Card* deck) {
     }
 }
 
+//Method to free the memory of a card from the deck by its index
 void removeCard(struct Card* deck, int index) {
     if(DEBUG == true) {
         printf("Removing card at address: %p...\n", deck + index);
@@ -265,12 +281,17 @@ void removeCard(struct Card* deck, int index) {
     }
 }
 
-void ShuffleDeck(struct Card* deck) {
+//function that randomly shuffle the deck
+void shuffleDeck(struct Card* deck) {
+    if(DEBUG == true) {
+        printf("Shuffling deck...");
+    }
+
     time_t t;
     srand((unsigned)time(&t));
     int loopCycles;
 
-    //Genera un numero random di cicli di shuffling
+    //Generate a random number of shuffling cycles
     do {
         loopCycles = rand() % 500;
     } while(loopCycles == 0);
@@ -279,7 +300,7 @@ void ShuffleDeck(struct Card* deck) {
         printf("Shuffling loop cycles: %d\n", loopCycles);
     }
 
-    //Per ogni ciclo di shuffling scorre tutte e quaranta le carte e le mischia
+    //For each shuffliing cycle shuffle all the deck's cards
     for(int l = 0; l <= loopCycles; l ++) {
         for(int i = 0; i < 40; i++) {
             int startPos;
@@ -309,9 +330,9 @@ void ShuffleDeck(struct Card* deck) {
     }
 
     if(DEBUG == true) {
-        printf("------------lista carte nello ShuffleDeck()-----------------\n");
+        printf("------------Cards list in ShuffleDeck()-----------------\n");
         for(int i = 0; i < 40; i++) {
-            printf("indirizzo: %p,\tSeme: %s,\tValore: %d\n", *(deck + i), getSeedName(*((deck + i)->seed)), (deck + i)->value);
+            printf("Address: %p,\tSeed: %s,\tValue: %d\n", deck + i, getSeedName(*((deck + i)->seed)), *((deck + i)->value));
         }
     }
 }
@@ -334,7 +355,8 @@ int main() {
             printf("Address: %p,\tSeed: %s,\tValue: %d\n", (deck + i), getSeedName((*((deck + i)->seed))), (*((deck + i)->value)));
         }
     }
+    shuffleDeck(deck);
 
-    disposeDeck(deck);
+    //disposeDeck(deck);
     return 0;
 }
